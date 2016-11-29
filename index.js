e0740d8aee49c2df17c0f7a875e947540847cf45
alsotang <alsotang@gmail.com>
2016-11-29 13:33:00 +0800

    统一文件页与 readme

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ var moment = require('moment');
 var pathLib = require('path');
 var chartsController = require('./controller/charts');
 var config = require('./config')
+var showdown = require('showdown')
+var mdConverter = new showdown.Converter({tables: true});
 
 var staticDir = pathLib.join(__dirname, './static');
 
@@ -29,27 +31,28 @@ app.use(bodyParser.json());
 
 app.post('/:chart_type', chartsController);
 
-var indexContent = generateIndexContent()
 app.get('/', function(req, res, next) {
-	res.send(indexContent)
+	var indexContent = fs.readFileSync(pathLib.join(staticDir, 'index.html'), 'utf-8')
+
+	res.send(_.template(indexContent)({
+		// config 必须有选择的暴露字段，不能全部暴露出去
+		config: {
+			backend_api: config.backend_api
+		}
+	}))
 });
 app.get('/doc', function(req, res, next) {
-    res.sendFile(pathLib.join(staticDir, 'doc.html'));
+	var docContent = fs.readFileSync(pathLib.join(staticDir, 'doc.html'), 'utf-8')
+	var readmeContent = fs.readFileSync(pathLib.join(__dirname, 'README.md'), 'utf-8')
+
+	res.send(_.template(docContent)({
+		readme: mdConverter.makeHtml(readmeContent)
+	}))
 });
 app.get('/about', function(req, res, next) {
     res.sendFile(pathLib.join(staticDir, 'about.html'));
 });
 app.use(express.static(staticDir));
-
-function generateIndexContent() {
-	var indexContent = fs.readFileSync(pathLib.join(staticDir, 'index.html'), 'utf-8')
-	return _.template(indexContent)({
-		// config 必须有选择的暴露字段，不能全部暴露出去
-		config: {
-			backend_api: config.backend_api
-		}
-	})
-}
 
 app.listen(config.port);
 console.log(`render server start at port ${config.port}`);
