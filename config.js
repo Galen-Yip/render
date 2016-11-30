acd09a967c8dabc9d76ae78f8e73dfb05fa07a28
Galen-Yip <275497985@qq.com>
2016-11-30 16:58:30 +0800

    delete dev info

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var backend_api = process.env.NODE_ENV == 'development' ? 'http://127.0.0.1:3334' : 'http://render.oa.com'
+var backend_api = 'http://render.oa.com'
 
 var config = {
 	backend_api: backend_api,
